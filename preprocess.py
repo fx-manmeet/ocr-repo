08919d2a9858a2b8ab3d976592f1f8349722af44
mann718 <mannmitansh@gmail.com>
2024-06-19 16:35:50 +0530

    With sharpening commit

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def save_image(image, filename):
     cv2.imwrite(filename, image)
@@ -23,12 +24,32 @@ def remove_noise(image):
     noise_removed = cv2.medianBlur(image, 5)
     return noise_removed
 
+def apply_sharpen(image):
+    kernel = np.array([[-1, -1, -1],
+                       [-1, 9,-1],
+                       [-1, -1, -1]])
+    sharpened = cv2.filter2D(image, -1, kernel)
+    return sharpened
+
+def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
 def preprocess_image_for_ocr(input_path):
     image = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
     blurred_image = apply_blur(image)
     thresholded_image = apply_threshold(blurred_image)
     morphed_image = apply_morphological_operations(thresholded_image)
-    final_image = remove_noise(morphed_image)
+    denoised_image = remove_noise(morphed_image)
+    final_image = unsharp_mask(denoised_image)
 
     file_name = os.path.basename(input_path)
     output_path = os.path.join('data', 'temp', f'temp-{file_name}')
